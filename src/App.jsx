81eb4e56c65d63b710f5b6e5792861d4d69d7574
CodeAritraDhank <aridhank21205@gmail.com>
2025-09-06 19:22:11 +0530

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -11,9 +11,21 @@ import Order from "./pages/admin/Order";
 import Cart from "./pages/client/Cart";
 import OrderHistory from "./pages/admin/OrderHistory";
 import CafeQRCode from "./components/CafeQRCode";
-// import { seedMenu } from "./services/seedMenu";
+import { useAuth } from "./context/useAuth";
+
+function ProtectedRoute({ children }) {
+  const { user } = useAuth();
+
+  if (!user) {
+    // not logged in → redirect to login
+    return <Navigate to="/login" replace />;
+  }
+
+  return children;
+}
 
 export default function App() {
+  // import { seedMenu } from "./services/seedMenu";
   //seed menu
   // useEffect(() => {
   //   seedMenu(); // run once
@@ -24,7 +36,14 @@ export default function App() {
       <Router>
         <Routes>
           <Route path="/" element={<HomePage />} />
-          <Route path="/admin/:cafeName/:cafeId" element={<AdminDashboard />}>
+          <Route
+            path="/admin/:cafeName/:cafeId"
+            element={
+              <ProtectedRoute>
+                <AdminDashboard />
+              </ProtectedRoute>
+            }
+          >
             <Route path="" element={<ViewMenu />} />
             <Route path="create" element={<CreateMenu />} />
             <Route path="order" element={<Order />} />
